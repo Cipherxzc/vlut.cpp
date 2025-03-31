f98888b9106322faccd8c5a5811e11a1777fdb38
Xiangyu Li <xiangyu.sdlc@foxmail.com>
2025-03-31 13:16:45 +0800

    Fix typo

--- evaluation/scripts/bench-all.sh
+++ evaluation/scripts/bench-all.sh
@@ -14,4 +14,4 @@ mv ~/repos/bitnet/llama.cpp/evaluation/results/* ~/repos/bitnet/llama.cpp-bitnet
 # Benchmark T-MAC
 ./bench-pp.sh -w ~/repos/bitnet/T-MAC/3rdparty/llama.cpp -m ~/models/bitnet_b1_58-3B/bitnet_b1_58-3B.INT_N.gguf -p 128,256,512 -t 1,2,4,8 -r 5 --csv
 # mv results back and merge with current
-mv ~/repos/bitnet/T-MAC/3rdparty/llama.cpp/evaluation/results* ~/repos/bitnet/llama.cpp-bitnet/evaluation/results/
+mv ~/repos/bitnet/T-MAC/3rdparty/llama.cpp/evaluation/results/* ~/repos/bitnet/llama.cpp-bitnet/evaluation/results/
