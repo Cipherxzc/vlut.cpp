35c4dd207563d07ec0f4ccca14d3ce3d53f2161a
Xiangyu Li <xiangyu.sdlc@foxmail.com>
2025-04-14 15:34:01 +0800

    Update bench-gemm.sh

--- evaluation/scripts/bench-gemm.sh
+++ evaluation/scripts/bench-gemm.sh
@@ -3,6 +3,10 @@
 # Get the directory where the script is located
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+mkdir -p $SCRIPT_DIR/../results_gemm
+
+> $SCRIPT_DIR/../results_gemm/results.txt
+
 # Change to the build/bin directory
 cd "$SCRIPT_DIR/../../build/bin"
 
@@ -10,11 +14,7 @@ cd "$SCRIPT_DIR/../../build/bin"
 for t in 1 4; do
     for m in bitnet_3b llama3_8b falcon_1b trilm_1.5b; do
         for n in 128 256 512 1024; do
-            ./test-bitnet-gemm perf -b CPU -t $t -m $m -ns $n
+            ./test-bitnet-gemm perf -b CPU -t $t -m $m -ns $n 2>&1 | grep "^  MUL_MAT" >> $SCRIPT_DIR/../results_gemm/results.txt
         done
     done
 done
-
-# Extract and save MUL_MAT lines to a file
-mkdir -p $SCRIPT_DIR/../results_gemm
-grep "^  MUL_MAT" > $SCRIPT_DIR/../results_gemm/results.txt
